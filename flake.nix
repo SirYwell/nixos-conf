f01bde058801c68290fdc95a8b8c9f85a5e2c7eb
SirYwell <hannesgreule@outlook.de>
2025-12-27 14:01:31 +0100

    Initial VR experiments

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
   };
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, nixpkgs-xr, ... }:
   let
     system = "x86_64-linux";
     pkgsUnstable = import nixpkgs-unstable { system = "x86_64-linux"; config.allowUnfree = true; };
@@ -15,6 +16,8 @@
         ./configuration.nix
 	./desktop.nix
 	./gaming.nix
+	./vr.nix
+	nixpkgs-xr.nixosModules.nixpkgs-xr
 	./rtx2070super.nix
       ];
       specialArgs = { inherit pkgsUnstable; };
---
+++ vr.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  ...
+}: {
+  services.monado = {
+    enable = true;
+    defaultRuntime = true;
+  };
+
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
+}

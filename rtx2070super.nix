60991a44c30e33a7474ca1a1b0e3debd776ccd17
SirYwell <hannesgreule@outlook.de>
2026-03-10 15:25:31 +0100

    VR stuff/unbreak nvidia(?)

--- rtx2070super.nix
+++ rtx2070super.nix
@@ -1,6 +1,12 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
 {
   hardware.graphics.enable = true;
-  hardware.nvidia.open = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
 }
 
--- vr.nix
+++ vr.nix
@@ -4,13 +4,13 @@
 }: {
   environment.systemPackages = [
     pkgs.openxr-loader
-    pkgs.wayvr
   ];
 
-  environment.sessionVariables = {
-    STEAMVR_RUNTIME = "1";
-    STEAMVR_LH_ENABLE = "1";
-    XRT_COMPOSITOR_COMPUTE = "1";
+  programs.steam = {
+    enable = true;
   };
+
+  hardware.steam-hardware.enable = true;
 }
+
 

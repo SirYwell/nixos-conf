677d2a882a8c6c4882fe1e72dff33ee4647d0055
SirYwell <hannesgreule@outlook.de>
2026-07-24 11:48:01 +0200

    JBR overlay temp fix

--- ides.nix
+++ ides.nix
@@ -3,11 +3,88 @@
   pkgs,
   ...
 }:
-let 
-  jetbrainsIde = ide: (pkgs.jetbrains."${ide}").override {
-    forceWayland = true;
+let
+  jetbrainsJdkPatched = pkgs.stdenv.mkDerivation rec {
+    pname = "jetbrains-jdk-bin";
+    version = "25.0.3-linux-x64-b329.124";
+
+    src = pkgs.fetchurl {
+      url = "https://cache-redirector.jetbrains.com/intellij-jbr/jbr_jcef-${version}.tar.gz";
+      hash = "sha256-0qUPSN+zV39BTp/A7OgOIEhs6hewqZwXxZUXXzE053U=";
+    };
+
+    nativeBuildInputs = [
+      pkgs.autoPatchelfHook
+    ];
+
+    buildInputs = with pkgs; [
+      alsa-lib
+      atk
+      at-spi2-atk
+      cairo
+      cups
+      dbus
+      fontconfig
+      freetype
+      glib
+      libdrm
+      libglvnd
+      libx11
+      libXcomposite
+      libXcursor
+      libXdamage
+      libxext
+      libXfixes
+      libXrandr
+      libxkbcommon
+      libxrender
+      libxtst
+      mesa
+      nspr
+      nss
+      pango
+      wayland
+      zlib
+    ];
+
+    runtimeDependencies = with pkgs; [
+      udev
+    ];
+
+    dontConfigure = true;
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r ./* $out/
+    '';
+
+    passthru.home = "${jetbrainsJdkPatched}";
   };
-in 
+
+  jetbrainsIde =
+    ide:
+    let
+      base = (pkgs.jetbrains."${ide}").override {
+        forceWayland = true;
+        jdk = jetbrainsJdkPatched;
+      };
+    in
+    pkgs.symlinkJoin {
+      name = "${ide}-jbr-patched";
+      paths = [ base ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram "$out/bin/${ide}" \
+          --set FONTCONFIG_FILE /etc/fonts/fonts.conf \
+          --set FONTCONFIG_PATH /etc/fonts \
+          --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [
+            pkgs.fontconfig
+            pkgs.freetype
+          ]}"
+      '';
+    };
+in
 {
   environment.systemPackages = [
     (jetbrainsIde "idea")
@@ -19,4 +96,3 @@ in
     pkgs.lazygit
   ];
 }
-
